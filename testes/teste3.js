b91407aa2e962ef1f0afa51fab2382f61b1280f3
lindomar <lindomar@mswi.com.br>
2021-01-17 12:44:29 -0300

    POO com classes - Heranças

--- testes/teste3.js
+++ testes/teste3.js
@@ -7,7 +7,7 @@ class Book{
         this.isbn = isbn;
     }
     printIbns(){
-        console.log(this.isbn)
+        console.log(this.isbn);
     }
 }
 
@@ -15,7 +15,35 @@ let book = new Book('title', 'pag', 'isbn');
 
 console.log(book.title);
 
-book.title = 'Novo Título'
+book.title = 'Novo Título';
 
-console.log(book) // Book { title: 'Novo Título', pages: 'pag', isbn: 'isbn' }
+console.log(book); // Book { title: 'Novo Título', pages: 'pag', isbn: 'isbn' }
 
+
+
+// Herança pg.63
+
+class ITBook extends Book{
+    constructor(title,pages,isbn,technology){
+        super(title,pages,isbn);
+        this.technology = technology;
+    }
+    printtechnology(){
+        console.log(this.technology);
+    }
+
+}
+
+let jsBook = new ITBook('Learning JA Algoriithms','200', '1234567890', 'JavaScript')
+
+console.log(jsBook) //
+/*
+ITBook {
+  title: 'Learning JA Algoriithms',
+  pages: '200',
+  isbn: '1234567890'
+}
+*/
+
+
+console.log(jsBook.printtechnology()) // JavaScript

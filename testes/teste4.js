24831759853cd86f83ef4e82bf522bb8a23861a6
lindomar <lindomar@mswi.com.br>
2021-01-17 12:54:51 -0300

    Trabalhando com getters e setters

---
+++ testes/teste4.js
@@ -0,0 +1,24 @@
+// Trabalhando com getters e setters
+class Person {
+    constructor(name){
+        this._name = name;
+    }
+    
+    get name(){
+        return this._name;
+    }
+
+    set name(value){
+        this._name = value;
+    }
+}
+
+let lotrChar = new Person('Froto');
+console.log(lotrChar.name);
+
+lotrChar.name = 'Gandalf';
+console.log(lotrChar.name);
+
+lotrChar._name = 'Sam';
+console.log(lotrChar.name)
+
